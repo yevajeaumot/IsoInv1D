581b8c0a9c226191ce677f0507dced4949dd5d97
Yéva Jeaumot <yevajeaumot@ige-glacio-p85.u-ga.fr>
2026-03-12 16:46:41 +0100

    age-depth profiles

--- age_model.py
+++ age_model.py
@@ -82,7 +82,9 @@ class RadarLine(object):
     def load_parameters(self):
 
         # parameters for all radar lines (file is mandatory - will overwrite default params)
-        data = yaml.load(open(self.label+'../parameters_all_radar_lines.yml').read(),
+        #data = yaml.load(open(self.label+'../parameters_all_radar_lines.yml').read(),
+                         #Loader=yaml.FullLoader)
+        data = yaml.load(open(self.label+'parameters_all_radar_lines.yml').read(),
                          Loader=yaml.FullLoader)
         if data != None:
             self.__dict__.update(data)
@@ -233,8 +235,8 @@ class RadarLine(object):
       
     def load_iso_ages(self):
        # Reading ages of isochrones and their sigmas
-        if os.path.isfile(self.label+'../ages.txt'):                # general age file
-            readarray = np.loadtxt(self.label+'../ages.txt')
+        if os.path.isfile(self.label+self.obs_iso_file):                # general age file
+            readarray = np.loadtxt(self.label+self.obs_iso_file)
         # if os.path.isfile(self.label+'ages.txt'):                   # specific to individual radar line
         #     readarray = np.loadtxt(self.label+'ages.txt')
         self.iso_obs_age = np.transpose([readarray[:, 0]])
@@ -1092,40 +1094,73 @@ class RadarLine(object):
         print('model displayed')
         
         #age depth profiles at ice cores locations 
-       
-        for name in self.ic: 
-            plt.figure(f'Age-Depth-{name}')
-            
-            z_obs, age_obs = np.loadtxt(self.ic[name]['obs'], skiprows=1, delimiter='\t', usecols=(0, 3), unpack=True)
-            plt.plot(age_obs/1000.0, z_obs, label=f'{name} obs.', color='k', linestyle='--', linewidth=2)
-           
-            idx_core = np.argmin(np.abs(self.distance - self.ic[name]['x']))
-            plt.plot(self.age[:, idx_core] / 1000., self.depth[:, idx_core], 
-                     label='Modeled age', color='b', linewidth=2)
-            
-            bedrock_depth = self.thkreal[idx_core]
-            plt.axhline(y= bedrock_depth, color='grey', linestyle='-', alpha=0.5, label='Bedrock')
-        
-            if hasattr(self, 'iso_obs_age'):
-                depths_at_core = self.iso[:, idx_core]
-                plt.errorbar(self.iso_obs_age[~np.isnan(depths_at_core)] / 1000.0, depths_at_core[~np.isnan(depths_at_core)],
-                                 xerr=self.iso_obs_age_sigma[~np.isnan(depths_at_core)] / 1000.0,
-                                 fmt='o', color='r', ecolor='r', capsize=3, markersize=4,
-                                 label='Radar Isochrones', zorder=5)
+        # age depth profiles at ice cores locations
 
-            plt.xlabel('age (ka)')
-            plt.ylabel('depth (m)')
-            plt.legend(loc='upper right') 
-            plt.grid(True, linestyle=':', alpha=0.6)
-            plt.axis((0, 1500, bedrock_depth +100, 0))
+        for name in self.ic:
             
+            if self.distance.min() <= self.ic[name]['x'] <= self.distance.max():
+        
+                idx_core = np.argmin(np.abs(self.distance - self.ic[name]['x']))
             
-            pp = PdfPages(self.label + 'AgeDepthIC.pdf')
-            pp.savefig(plt.figure('Age-Depth-IC'))
-            pp.close()
-            plt.close('Age-Depth-IC')
-    
+                depth_model = self.depth[:, idx_core]
+                age_model = self.age[:, idx_core]
+                tau_model = self.tau[:, idx_core]
+                sigma_model = self.sigma_age[:, idx_core]
             
+                bedrock_depth = self.ic[name]['max_depth']
+            
+                fig = plt.figure(f'Age-Depth-{name}')
+                
+                plt.plot(age_model/1000., depth_model, color='b', linewidth=2, label='Model')
+            
+                obs_file = self.ic[name].get('obs')
+            
+                if obs_file is not None:
+            
+                    z_obs, age_obs = np.loadtxt(obs_file, skiprows=1, delimiter='\t', usecols=(0,1),
+                        unpack=True)
+            
+                    plt.plot(age_obs/1000., z_obs, '--k', linewidth=2, label=f'{name} obs.')
+            
+                if hasattr(self, 'iso_obs_age'):
+            
+                    depths_at_core = self.iso[:, idx_core]
+                    mask = ~np.isnan(depths_at_core)
+            
+                    plt.plot(self.iso_obs_age[mask]/1000., depths_at_core[mask],
+                             'ro', markersize=4,
+                             label='Obs Isochrones')
+            
+                plt.axhline(y=bedrock_depth, color='grey', linestyle='-', alpha=0.5, label='Bedrock')
+            
+                plt.xlabel('Age (ka)')
+                plt.ylabel('Depth (m)')
+                plt.legend(loc='upper right')
+                plt.grid(True, linestyle=':', alpha=0.6)
+            
+                plt.axis((0,1500,bedrock_depth+100,0))
+            
+                pdf_name = f"{self.label}AgeDepth_{name}.pdf"
+            
+                with PdfPages(pdf_name) as pp:
+                    pp.savefig(fig)
+            
+                plt.close(fig)
+            
+                header_text = (f"# Total ice thickness (m): {bedrock_depth}\n"
+                               f"# Age at bedrock (yr): {age_model[-1]}\n"
+                               f"# Ice core location (km): {self.ic[name]['x']}\n")
+            
+                output = np.column_stack((depth_model, age_model, sigma_model, tau_model))
+            
+                txt_name = f"{self.label}AgeDepth_{name}.txt"
+            
+                np.savetxt(txt_name,
+                           output,
+                           delimiter="\t",
+                           header=header_text + "depth(m)\tage(yrs)\tsigma_age\tthinning",
+                           comments="")
+        
     # model parameter graphs
     def parameters_display(self):
 
@@ -1176,12 +1211,19 @@ class RadarLine(object):
         omega_drill = np.array([ np.interp(distance_drill, self.distance, self.omega[dep]) for dep in range(len(self.omega)) ])
         age_density_drill = np.array([ np.interp(distance_drill, self.distance, self.age_density[dep]) for dep in range(len(self.age_density)) ])
         # single values at drill location
-        R_AICC2023 = self.AICC2023_accu/self.AICC2023_averageaccu
-        f = self.interp1d_lin_aver(self.AICC2023_age, R_AICC2023, right=1., left=R_AICC2023[0])
-        R_drill = f(age_drill)
-        a_drill = np.interp(distance_drill,self.distance, self.a)
-        accu_drill = a_drill*R_drill
-        accu_drill = np.concatenate((accu_drill,np.array([np.nan])))
+        
+        #R_AICC2023 = self.AICC2023_accu/self.AICC2023_averageaccu
+        #f = self.interp1d_lin_aver(self.AICC2023_age, R_AICC2023, right=1., left=R_AICC2023[0])
+        #R_drill = f(age_drill)
+        #a_drill = np.interp(distance_drill,self.distance, self.a)
+        #accu_drill = a_drill*R_drill
+        
+        R_drill = np.interp(age_drill, self.age_R, self.R)
+        a_drill = np.interp(distance_drill, self.distance, self.a)
+        accu_drill = a_drill * R_drill
+        
+        
+        #accu_drill = np.concatenate((accu_drill,np.array([np.nan])))
         bed_drill = np.interp(distance_drill, self.distance, self.thkreal)
         stag_drill = np.interp(distance_drill, self.distance, self.stagnant)
         stigmastag_drill = np.interp(distance_drill, self.distance, self.sigma_thk)
@@ -1222,7 +1264,8 @@ class RadarLine(object):
         vv_plot, ax1 = plt.subplots()
         vv = -accu_now*omega_drill      #vertical velocity
         ax1.plot(vv, depth_drill)
-        ax1.plot(-accu_drill*omega_drill , depth_drill)
+        #ax1.plot(-accu_drill*omega_drill , depth_drill)
+        ax1.plot(-accu_drill * omega_drill , depth_drill)
         ax1.set_xlabel('vertical velocity (m/yr)')
         ax1.set_ylabel('depth (m)')
         ax1.invert_yaxis()
@@ -1371,9 +1414,14 @@ class RadarLine(object):
         self.parameters_display()
 
         # save data for drills
-        if self.is_EDC: self.drill('EDC', self.distance_EDC)
-        if self.is_BELDC: self.drill('BELDC', self.distance_BELDC)
-        if self.is_core: self.drill(self.name_core, self.distance_core)
+        #if self.is_EDC: self.drill('EDC', self.distance_EDC)
+        #if self.is_BELDC: self.drill('BELDC', self.distance_BELDC)
+        #if self.is_core: self.drill(self.name_core, self.distance_core)
+        
+        for name in self.ic:
+            xcore = self.ic[name]['x']
+            if self.distance.min() <= xcore <= self.distance.max():
+                self.drill(name, xcore)
 
 
 # get radar line name from terminal and run
